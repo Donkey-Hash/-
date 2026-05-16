ac3d15b76eebc3f495b8afa269cec8cc7e7670e0
xtx <1439031060@qq.com>
2026-05-16 21:13:26 +0800

    Remove obsolete files related to valve and ysspectro components
    - Deleted binary object files: usart.o, valve.o, ysspectro.o, and yssyringepump.o.
    - Removed corresponding dependency files: valve.crf, valve.d, ysspectro.crf, ysspectro.d, yssyringepump.crf, and yssyringepump.d.
    - Cleaned up unnecessary files to streamline the project structure and reduce clutter.

--- 0429重新/nanoWAys20240724改单片机+测润泽/mbfuncdiag.d
+++
@@ -1,1 +0,0 @@
-..\mbfuncdiag.o: ..\FreeModbus\modbus\functions\mbfuncdiag.c
--- 0429重新/nanoWAys20240724改单片机+测润泽/pid.d
+++
@@ -1,3 +0,0 @@
-..\pid.o: ..\Bsp\src\pid.c
-..\pid.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
-..\pid.o: ../Bsp/inc/pid.h
